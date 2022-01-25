cee23bba8fd45afefd8f5d5242da6bfb9774943c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-01-25 15:49:49 +0200

    functions

---
+++ 25.01/index.js
@@ -0,0 +1,53 @@
+// alert()
+// let x = confirm(`tratratrta`)
+// console.log(x)
+// let x = prompt(`text`, 0)
+// console.log(x)
+
+//FUNCTIONS
+//regular
+// function fun() {
+// console.log(`fun`)
+// return 1;
+// }
+
+//arrow
+// const/let
+// const fun1 = () => {
+//     console.log(`fun1`)
+// }
+
+// console.log(fun())
+// fun1()
+
+//anonimus
+// () => {
+//     console.log(`fun3`)
+// };
+
+// IIFE
+// ((a) => {
+//     console.log(a)
+// })('9')
+
+//LOOPS
+//FOR
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+//WHILE
+// let i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     if (i == 7) {
+//         console.log(`boom`)
+//     }
+//     i++
+// }
